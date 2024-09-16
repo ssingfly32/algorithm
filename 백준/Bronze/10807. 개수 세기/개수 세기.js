8600711e617cdb11508fd5f1cffbf26688d8954f
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2024-09-16 18:20:14 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.js
+++ 백준/Bronze/10807. 개수 세기/개수 세기.js
@@ -1,9 +1,6 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const num = parseInt(input[0]);
 const findNum = parseInt(input[2]);
 const numArr = input[1].split(' ').map(Number);
-let dupCount = 0;
-numArr.forEach(el=> {
-    el === findNum ? dupCount++ : '';
-})
+const dupCount = numArr.filter(num => num === findNum).length;
+
 console.log(dupCount);

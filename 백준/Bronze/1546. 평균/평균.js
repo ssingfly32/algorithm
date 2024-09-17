39382aadb102b2a0771e951109104d86fd254a36
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2024-09-17 22:00:49 +0900

    [Bronze I] Title: 평균, Time: 92 ms, Memory: 9640 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.js
+++ 백준/Bronze/1546. 평균/평균.js
@@ -2,7 +2,6 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('
 const subjectNum = parseInt(input[0]);
 const scores = input[1].split(' ').map(Number);
 const maxScore = Math.max(...scores);
-const maxScoreIdx = scores.indexOf(maxScore);
 scores.forEach((el,idx) => scores[idx] = Number((scores[idx] / maxScore * 100).toFixed(2)));
 const answer = Number((scores.reduce((acc, cur) => acc + cur) / subjectNum).toFixed(2));
 console.log(answer);

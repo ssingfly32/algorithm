f007ab86a1244eb872861182c37cad727067bcff
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2024-11-05 16:01:19 +0900

    [Silver V] Title: 소트인사이드, Time: 100 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.js
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -1,20 +1,5 @@
-const inputs = require('fs').readFileSync('/dev/stdin').toString().trim().split('').map(Number);
+const inputArr = require('fs').readFileSync('/dev/stdin').toString().trim().split('').map(Number);
 
-for (let i = 0; i < inputs.length; i++) {
-    const num = inputs[i];
-    let maxNum = num;
-    let maxNumIdx = i;
-    for (let j = i + 1; j < inputs.length; j++) {
-        if (maxNum < inputs[j]) {
-            maxNum = inputs[j];
-            maxNumIdx = j;
-        } 
-    }
-    
-    if (maxNumIdx !== i) {
-        [inputs[i], inputs[maxNumIdx]] = [inputs[maxNumIdx], inputs[i]];
-    }
-    
-}
+const sortedInput = inputArr.sort((a, b) => b - a);
 
-console.log(inputs.join(''));
+console.log(sortedInput.join(''));

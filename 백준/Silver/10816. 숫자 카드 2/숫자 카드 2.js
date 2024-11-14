85d4ba206d8d895316b166d7d6b8e5186d62bbb3
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2024-11-14 17:29:39 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 752 ms, Memory: 159636 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -1,14 +1,29 @@
 const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
 
-const cards = input[1].split(' ').map(Number);
-const targetCards = input[3].split(' ').map(Number);
-const cardMap = new Map();
-cards.forEach(v => {
-    if (cardMap.has(v)) {
-        let count = cardMap.get(v);
-        cardMap.set(v, count + 1);
-        return;
+const sCardCnt = input[0];
+const sCardList = input[1].split(' ')
+const aCardCnt = input[2];
+const aCardList = input[3].split(' ');
+const matchingCntList = new Array(input[2]);
+
+const sCardMap = new Map();
+sCardList.forEach(sCard => {
+    let sCardCnt = sCardMap.get(sCard);
+    if(sCardCnt) {
+        sCardCnt++;
+    } else {
+        sCardCnt = 1;
     }
-    cardMap.set(v, 1);
-});
-console.log(targetCards.map(v => cardMap.get(v) ? cardMap.get(v) : 0).join(' '));
+    sCardMap.set(sCard, sCardCnt);
+})
+
+aCardList.map((aCard, idx)=> {
+    const matchingCnt = sCardMap.get(aCard)
+    if(matchingCnt) {
+        matchingCntList[idx] = matchingCnt;
+    } else {
+        matchingCntList[idx] = 0;
+    }
+    
+})
+console.log(matchingCntList.join(' '));

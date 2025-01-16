dcd895edecfad1104892a27959dbc3e70dc161d1
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2025-01-16 23:45:30 +0900

    [level 1] Title: 대충 만든 자판, Time: 3.86 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/README.md
+++ 프로그래머스/1/160586. 대충 만든 자판/README.md
@@ -1,10 +1,10 @@
 # [level 1] 대충 만든 자판 - 160586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586?language=javascript) 
 
 ### 성능 요약
 
-메모리: 34.4 MB, 시간: 3.76 ms
+메모리: 34.4 MB, 시간: 3.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 16일 23:32:50
+2025년 01월 16일 23:45:28
 
 ### 문제 설명
 
--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,4 +1,5 @@
 function solution(keymap, targets) {
+    const answer = [];
     const map = new Map();
     for (const keys of keymap) {
         for (const [i, key] of [...keys].entries()) {
@@ -7,7 +8,16 @@ function solution(keymap, targets) {
             }
         }
     }
-    return targets.map(v => [...v].reduce((pre, cur) => { 
-        return pre === -1 || !map.has(cur) ? -1 : pre + map.get(cur);
-    }, 0));
+    for (const [idx, target] of targets.entries()) {
+        let count = 0;
+        for (const char of [...target]) {
+            if (!map.get(char)) {
+                count = -1;
+                break;
+            }
+            count += map.get(char);
+        }
+        answer[idx] = count;
+    }
+    return answer;
 }

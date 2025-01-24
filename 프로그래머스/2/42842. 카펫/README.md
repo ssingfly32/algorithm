5eafacdf3fe6a7b828c03a75b0d09477626cb2b9
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2025-01-24 18:48:18 +0900

    [level 2] Title: 카펫, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.26 ms
+메모리: 33.6 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 24일 18:31:00
+2025년 01월 24일 18:48:16
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,11 +1,11 @@
 function solution(brown, yellow) {
-    let temp = brown / 2 - 1;
-    for (let i = 1; i < brown / 2; i++) {
-        if (temp >= (brown - temp * 2) / 2 + 2) {
-            if ((temp - 2) * i === yellow) {
-                return [temp, (brown - temp * 2) / 2 + 2];
+    const total = brown + yellow;
+    for (let height = 3; height <= Math.sqrt(total); height++) {
+        if (total % height === 0) {
+            const width = total / height;
+            if (width >= height && (width - 2) * (height - 2) === yellow) { 
+                return [width, height];
             }
         }
-        temp--;
     }
 }

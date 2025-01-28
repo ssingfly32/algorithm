a77a523597729e4ce9fce8b7378dd50f48617cf4
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2025-01-28 19:12:55 +0900

    [level 1] Title: 소수 찾기, Time: 52.66 ms, Memory: 45 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    const arr = new Array(n + 1).fill(true);
+    arr[0] = arr[1] = false;
+    for (let i = 2; i * i <= n; i++) {
+       if (arr[i]) {
+           for (let j = i * i; j <= n; j += i) {
+               arr[j] = false;
+           }
+       }
+    }
+    return arr.filter(v => v).length;
+}

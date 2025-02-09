04fe44b21afafed527d720591552ac5d3ef4de80
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2025-02-09 21:17:59 +0900

    [level 1] Title: 신고 결과 받기, Time: 215.61 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/README.md
+++ 프로그래머스/1/92334. 신고 결과 받기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=javascript) 
 
 ### 성능 요약
 
-메모리: 106 MB, 시간: 323.13 ms
+메모리: 102 MB, 시간: 215.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 09일 21:08:36
+2025년 02월 09일 21:17:59
 
 ### 문제 설명
 
--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -3,15 +3,15 @@ function solution(id_list, report, k) {
     const userToBeSuspended = new Map();
     // 유저들의 신고 당한 횟수를 담은 맵
     for (const [reporter, reportedU] of reportArr) {
-        userToBeSuspended.has(reportedU) ? userToBeSuspended.set(reportedU, userToBeSuspended.get(reportedU) + 1) : userToBeSuspended.set(reportedU, 1);
+        userToBeSuspended.set(reportedU, userToBeSuspended.get(reportedU) + 1 || 1);
     }
     
-    const reporterObj = {};
+    const reporterObj = new Map();
     // key: 신고자, value: 정지될 유저(k번 이상 신고당한 유저) 배열
     for (const [reporter, reportedU] of reportArr) {
-        if (userToBeSuspended.has(reportedU) && userToBeSuspended.get(reportedU) >= k) {
-            reporterObj[reporter] ? reporterObj[reporter] = [...reporterObj[reporter], reportedU] : reporterObj[reporter] = [reportedU];
+        if (userToBeSuspended.get(reportedU) >= k) {
+            reporterObj.set(reporter, reporterObj.get(reporter) + 1 || 1);
         }
     }
-    return id_list.map(id => reporterObj[id] ? reporterObj[id].length : 0);
+    return id_list.map(id => reporterObj.get(id) || 0);
 }

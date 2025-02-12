d966321c145a81571e186951bac26b1744e7ae7e
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2025-02-12 23:39:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 30.64 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,7 +1,15 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-    for (const [i, person] of participant.entries()) {
-        if (person !== completion[i]) return person;
-    }
+    // participant.sort();
+    // completion.sort();
+    // for (const [i, person] of participant.entries()) {
+    //     if (person !== completion[i]) return person;
+    // }
+    
+    // 해시로 풀기
+    const map = new Map();
+    let answer = '';
+    participant.forEach(v => map.set(v, map.get(v) + 1 || 1));
+    completion.forEach(v => map.set(v, map.get(v) - 1));
+    map.forEach((v, k) => v ? answer = k : '');
+    return answer;
 }

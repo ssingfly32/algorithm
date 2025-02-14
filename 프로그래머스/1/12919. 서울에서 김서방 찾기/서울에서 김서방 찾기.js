357500d5a0241a35b73b247b35e6e84e2c9bfd65
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2025-02-14 19:41:14 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,5 +1,3 @@
 function solution(seoul) {
-    for (const [i, name] of seoul.entries()) {
-        if (name === 'Kim') return `김서방은 ${i}에 있다`;
-    }
+    return `김서방은 ${seoul.findIndex(name => name ==='Kim')}에 있다`;
 }

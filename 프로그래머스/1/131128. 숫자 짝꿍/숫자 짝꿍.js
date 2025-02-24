ec6f585a30aca0e3d9feb6594ca80ff36aaccdd8
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2025-02-25 00:41:38 +0900

    [level 1] Title: 숫자 짝꿍, Time: 276.72 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,27 @@
+function solution(X, Y) {
+    const matchedNums = Array.from(new Array(10), () => new Array(2).fill(null));
+    const x = X+'';
+    const y = Y+'';
+    let lastNum = 9;
+    for (let i = 0; i < 10; i++) {
+        const xCnt = findTarget(lastNum, x);
+        const yCnt = findTarget(lastNum, y);
+        matchedNums[i][0] = lastNum--;
+        matchedNums[i][1] = xCnt <= yCnt ? xCnt : yCnt;
+    }    
+    let answer = '';
+    matchedNums.forEach(v => v[1] > 0 ? answer += (v[0] + '').repeat(v[1]) : '');
+    if (answer !== '' && [...answer].every(v => v === '0')) return '0';
+    
+    return answer !== '' ? answer : '-1'; 
+}
+
+function findTarget(target, str) {    
+    let currentIdx = -1;
+    let count = 0;
+    do {
+        currentIdx = str.indexOf(target, currentIdx + 1);
+        if (currentIdx > -1) count++;
+    } while (currentIdx > -1)
+    return count;
+}

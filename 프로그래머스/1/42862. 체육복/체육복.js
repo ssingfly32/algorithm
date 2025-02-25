6a29b34c42fd5acfe8d785332512a9effd6bb5e7
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2025-02-26 06:12:07 +0900

    [level 1] Title: 체육복, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,19 @@
+function solution(n, lost, reserve) {
+    const clothCnt = new Array(n).fill(1);
+    for (let i = 0; i < n; i++) {
+        if (lost.indexOf(i + 1) > -1) clothCnt[i] -= 1;
+        if (reserve.indexOf(i + 1) > -1) clothCnt[i] += 1;
+    }
+    const answer = clothCnt.forEach((v, idx) => {
+        if (!v) {
+            if (idx && clothCnt[idx - 1] > 1) {
+                clothCnt[idx - 1] -= 1;
+                clothCnt[idx]++;
+            } else if (idx !== n - 1 && clothCnt[idx + 1] > 1) {
+                clothCnt[idx + 1] -= 1;
+                clothCnt[idx]++;
+            }
+        }
+    });
+    return clothCnt.reduce((acc, v) => acc + (v > 0 ? 1 : 0), 0);
+}

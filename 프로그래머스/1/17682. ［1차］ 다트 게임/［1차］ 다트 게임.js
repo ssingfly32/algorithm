28911de4204c92e1fc412f0c3fa2c506330fa5c4
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2025-03-26 14:15:12 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,40 @@
+function solution(dartResult) {
+    // 총 3번의 기회
+    // 각 기회마다 0점에서 10점까지이다.
+    // S: 1제곱, D: 2제곱, T: 3제곱
+    // * 스타상: 해당점수와 바로전에 얻은 점수 2배, # 아차상: 해당 점수 -
+    // buffer: 게임 마다 저장해서 이전 게임 알 수 있게끔.
+    // 0점부터 10점까지 있기 때문에 10 주의.
+    
+    let intBuffer = '';
+    const stack = [];
+    const bonus = {'S': 1, 'D': 2, 'T': 3};
+    for (let i = 0; i < dartResult.length; i++) {
+        // 숫자일 때
+        if (!isNaN(dartResult[i])) {
+            intBuffer += dartResult[i];
+        } else if (isNaN(dartResult[i])) { // 숫자가 아닐 때
+            // 숫자가 아니고 S, D, T중 하나일 때 점수 계산
+            if (dartResult[i] in bonus) {
+                stack.push(Number(intBuffer) ** bonus[dartResult[i]]);
+            } else {
+                const len = stack.length;
+                if (dartResult[i] === '*') {    // 옵션 *
+                    stack[len - 1] = stack[len - 1] * 2;
+                    // 이전 게임이 있다면 이전 게임에도 * 2
+                    if (len > 1) {
+                        stack[len - 2] = stack[len - 2] * 2;
+                    }
+                } else {
+                    // 옵션이 #일 때
+                    stack[len - 1] = stack[len - 1] * -1;
+                }
+            }
+            // 다음 문자가 숫자일 시 
+            if (!isNaN(dartResult[i + 1])) {
+                intBuffer= '';
+            }
+        }
+    }
+    return stack.reduce((acc, cur) => acc + cur, 0);
+}

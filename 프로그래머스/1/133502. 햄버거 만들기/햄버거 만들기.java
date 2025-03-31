e71f9ae2605ea5e82816cb6d360a4dbcf31f1939
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2025-03-31 14:53:01 +0900

    [level 1] Title: 햄버거 만들기, Time: 71.67 ms, Memory: 151 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    int[] order = {1, 2, 3, 1};
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int el : ingredient) {
+            stack.push(el);
+            int size = stack.size();
+            if (size >= 4) {
+                if (stack.get(size - 4) == 1 && 
+                    stack.get(size - 3) == 2 && 
+                    stack.get(size - 2) == 3 && 
+                    stack.get(size - 1) == 1) 
+                {
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    answer++;
+                }                                               
+            }
+        }
+        return answer;
+    }
+
+}

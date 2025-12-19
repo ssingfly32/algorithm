a185f9a97e0a1927001292c73f8748a6d4e6c7e3
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2025-12-19 22:12:47 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1524 ms, Memory: 97440 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static ArrayList<Integer>[] graph;
+    static int[] visited;
+    static int visitOrder = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n + 1];
+        // 우선 그래프 초기화
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        // 간선 입력
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        // 정점 번호 작은 것부터 방문하기 위해 정렬
+        for (int i = 1; i<= n; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        visited = new int[n + 1];
+
+        dfs(r);
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(visited[i]);
+        }
+    }
+
+    private static void dfs(int r) {
+        visitOrder++;
+        visited[r] = visitOrder;
+
+        for (int next : graph[r]) {
+            if (visited[next] == 0) {
+                dfs(next);
+            }
+        }
+    }
+}

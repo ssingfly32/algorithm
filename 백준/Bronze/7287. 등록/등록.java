e01fe8cfb47a0f2f7edb8b9cbf903159a8cf3f01
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2025-12-30 20:14:44 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("169").append("\n").append("ssingfly32");
+        System.out.println(sb);
+    }   
+}

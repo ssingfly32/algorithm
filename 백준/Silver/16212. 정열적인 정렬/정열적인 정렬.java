f951d147dce909452fc5a6ceb5f03bef76e08007
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2026-01-14 22:03:51 +0900

    [Silver V] Title: 정열적인 정렬, Time: 1220 ms, Memory: 80496 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : arr) {
+            sb.append(num).append(" ");
+        }
+        System.out.println(sb);
+    }
+}

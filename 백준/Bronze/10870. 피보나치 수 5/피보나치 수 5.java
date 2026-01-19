0642c6f37caadce1f19e52e32ce42177e3a18f68
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2026-01-19 22:48:16 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = fibonacci(N);
+        System.out.println(answer);
+    }
+
+    public static int fibonacci(int n){
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}

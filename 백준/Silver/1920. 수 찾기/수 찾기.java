3c4a7c1a59c43a93606ac80e9f88d5cd3f21622f
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2026-01-21 23:03:25 +0900

    [Silver IV] Title: 수 찾기, Time: 560 ms, Memory: 47824 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,17 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<Integer> set = new HashSet<>();
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        arr = new int[n];
 
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            set.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(arr);
 
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
@@ -20,22 +18,8 @@ public class Main {
 
         for (int i = 0; i < m; i++) {
             int target = Integer.parseInt(st.nextToken());
-            int result = binarySearch(target);
-            sb.append(result).append("\n");
+            sb.append(set.contains(target) ? 1 : 0).append("\n");
         }
         System.out.println(sb);
-    }
-
-    private static int binarySearch(int target) {
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] == target) return 1;
-            if (arr[mid] < target) left = mid + 1;
-            else right = mid - 1;
-        }
-        return 0;
     }
 }

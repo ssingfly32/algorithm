c200e1d43d37081f91f79342552c6311561163c4
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2026-02-11 19:17:16 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,29 +1,27 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] counts = new int[n + 1];
-        Set<Integer> reserveSet = 
-            Arrays.stream(reserve).boxed().collect(Collectors.toSet());
-        Set<Integer> lostSet = 
-            Arrays.stream(lost).boxed().collect(Collectors.toSet());
+        int[] counts = new int[n + 2];
         
         for (int i = 1; i <= n; i++) {
-            if (!lostSet.contains(i)) {
-                counts[i] = 1;
-            }
-            if (reserveSet.contains(i)) {
-                counts[i]++;
-            }
+            counts[i] = 1;
+        }
+        
+        for (int num : lost) {
+            counts[num]--;
+        }
+        
+        for (int num : reserve) {
+            counts[num]++;
         }
         
         for (int i = 1; i <= n; i++) {
             if (counts[i] == 0) {
-                if (counts[i - 1] == 2) {
+                if (counts[i - 1] > 1) {
                     counts[i - 1]--;
                     counts[i]++;
-                } else if (i < n && counts[i + 1] == 2) {
+                } else if (counts[i + 1] > 1) {
                     counts[i + 1]--;
                     counts[i]++;
                 }
@@ -31,12 +29,9 @@ class Solution {
         }
         
         int answer = 0;
-        
-        // 체육수업 들을 수 있는 학생 수 찾기
-        for (int count : counts) {
-            if (count > 0) answer++;
+        for (int i = 1; i <= n; i++) {
+            if (counts[i] > 0) answer++;   
         }
-        
         return answer;
     }
 }

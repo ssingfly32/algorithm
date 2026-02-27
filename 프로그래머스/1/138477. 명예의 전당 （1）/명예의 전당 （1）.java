22c7334e343ba8ae255b6fc65e047d49679228e9
ssingfly32 <68856397+ssingfly32@users.noreply.github.com>
2026-02-27 17:14:00 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.44 ms, Memory: 97.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[] answer = new int[score.length];
+        
+        for (int i = 0; i < score.length; i++) {
+            pq.add(score[i]);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+            answer[i] = pq.peek();
+        }
+        
+        return answer;
+    }
+}
